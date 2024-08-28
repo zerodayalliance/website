366cd146c8f2d46883330e151e6267b1e1af86ac
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-08-29 01:09:45 +0530

    added path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} /> 
         </Routes>
       </BrowserRouter>
     </>
