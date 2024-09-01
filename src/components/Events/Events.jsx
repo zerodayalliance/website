0c9a7e4625317718d7135af0dca73b79a7750a9f
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-09-02 01:42:00 +0530

    added Heading

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,5 +1,6 @@
 import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
 import image from "/src/assets/null.jpeg";
+import { Title} from '@mantine/core';
 import {
   Card,
   Image,
@@ -19,22 +20,22 @@ const eventData = [
     name: 'upcoming',
     time: 'NA',
     meetingLink: '#',
-    mode: 'Online',
+    mode: 'NA',
     speakerName: 'NA',
   },
   {
     name: 'To be announced',
-    time: '4:00 PM',
-    meetingLink: 'https://example.com/ai-conference',
+    time: 'NA',
+    meetingLink: 'NA',
     mode: 'Offline',
-    speakerName: 'Jane Doe',
+    speakerName: 'NA',
   },
   {
     name: 'To be announced',
-    time: '6:00 PM',
-    meetingLink: 'https://example.com/web-dev-bootcamp',
+    time: 'NA',
+    meetingLink: 'NA',
     mode: 'Online',
-    speakerName: 'John Smith',
+    speakerName: 'NA',
   },
 ];
 
@@ -43,61 +44,66 @@ const Events = () => {
   const theme = useMantineTheme();
 
   return (
-    <Group className={classes.cardsContainer}>
-      {eventData.map((event, index) => (
-        <Card key={index} withBorder radius="md" className={classes.card}>
-          <Card.Section>
-            <a {...linkProps}>
-              <Image src={image} height={200}  fit="cover" />
-            </a>
-          </Card.Section>
+    <>
+       <Title ta="center" className={classes.title1}>
+          Upcoming Events
+        </Title>
+      <Group className={classes.cardsContainer}>
+        {eventData.map((event, index) => (
+          <Card key={index} withBorder radius="md" className={classes.card}>
+            <Card.Section>
+              <a {...linkProps}>
+                <Image src="https://media1.tenor.com/m/-4_cFnz_D3YAAAAC/btc-mechanics-ordinals.gif" height={200} fit="cover" />
+              </a>
+            </Card.Section>
 
-          <Text className={classes.title} fw={500} component="a" {...linkProps}>
-            {event.name}
-          </Text>
+            <Text className={classes.title} fw={500} component="a" {...linkProps}>
+              {event.name}
+            </Text>
 
-          <Group justify="space-between" className={classes.footer}>
-            <Center>
-              <Avatar
-                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png"
-                size={34}
-                radius="xl"
-                mr="xs"
-              />
-              <Text fz="sm" inline >
-                {event.speakerName}
-              </Text>
-            </Center  >
+            <Group justify="space-between" className={classes.footer}>
+              <Center>
+                <Avatar
+                  src=""
+                  size={34}
+                  radius="xl"
+                  mr="xs"
+                />
+                <Text fz="sm" inline>
+                  {event.speakerName}
+                </Text>
+              </Center>
 
-            <Group gap={8} mr={0}>
-              <ActionIcon className={classes.action} style={{ backgroundColor: 'black' }}>
-                <IconBrandLinkedin style={{ width: rem(16), height: rem(16), color: 'white' }} />
-              </ActionIcon>
-              <ActionIcon className={classes.action} style={{ backgroundColor: 'black' }}>
-                <IconBrandGithub style={{ width: rem(16), height: rem(16), color: 'white' }} />
-              </ActionIcon>
+              <Group gap={8} mr={0}>
+                <ActionIcon className={classes.action} style={{ backgroundColor: 'black' }}>
+                  <IconBrandLinkedin style={{ width: rem(16), height: rem(16), color: 'white' }} />
+                </ActionIcon>
+                <ActionIcon className={classes.action} style={{ backgroundColor: 'black' }}>
+                  <IconBrandGithub style={{ width: rem(16), height: rem(16), color: 'white' }} />
+                </ActionIcon>
+              </Group>
             </Group>
-          </Group>
 
-          <Text fz="sm" c="dimmed" lineClamp={4}>
-            Date: {event.time}
-          </Text>
-          <Text fz="sm" c="dimmed" lineClamp={4}>
-            Mode: {event.mode}
-          </Text>
-          <Button
-            component="a"
-            href={event.meetingLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            variant="outline"
-            style={{ width: '100%' }}
-          >
-            Join
-          </Button>
-        </Card>
-      ))}
-    </Group>
+            <Text fz="sm" c="dimmed" lineClamp={4}>
+              Date: {event.time}
+            </Text>
+            <Text fz="sm" c="dimmed" lineClamp={4}>
+              Mode: {event.mode}
+            </Text>
+            <Button
+              component="a"
+              href={event.meetingLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline"
+              style={{ width: '100%' }}
+            >
+              Join
+            </Button>
+          </Card>
+        ))}
+      </Group>
+    </>
   );
 }
 
