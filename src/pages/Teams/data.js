065ec7487e000e45812cf454cb85c882deba2ae5
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-09-02 00:45:53 +0530

    add teams page

---
+++ src/pages/Teams/data.js
@@ -0,0 +1,146 @@
+// M: 1 2 3            9 10
+// F:       4 5 6 7 8
+
+const teams = [
+  {
+    name: "Lead",
+    members: [
+      {
+        name: "Dibyataru Chakraborty",
+        role: "Community Lead",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png",
+      },
+    ],
+  },
+  {
+    name: "Tech Team (Cyber Security)",
+    members: [
+      {
+        name: "Soumyadeep Dutta",
+        role: "Cyber Security Lead",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-9.png",
+      },
+      {
+        name: "Avik Samanta",
+        role: "Cyber Security, Shell",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png",
+      },
+      {
+        name: "Debangshu Roy",
+        role: "Cyber Security, Network",
+        bio: "I'm passionate about Ethical Hacking and Red Teaming.",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-9.png",
+      },
+      {
+        name: "Debankan Mullick",
+        role: "Cyber Security, Shell",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-10.png",
+      },
+      {
+        name: "Mohammed Zoheb",
+        role: "Cyber Security",
+        bio: "Cyber Security Enthusiast.",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png",
+      },
+      {
+        name: "Praveen Joshi",
+        role: "Cyber Security, Network",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-10.png",
+      },
+      {
+        name: "Rajdeep Saha",
+        role: "Cyber Security, Crypto",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-3.png",
+      },
+      {
+        name: "Soumi Biswas",
+        role: "Cyber Security, Crypto",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-4.png",
+      },
+    ],
+  },
+  {
+    name: "Tech Team (Web Development)",
+    members: [
+      {
+        name: "Krishnendu Das",
+        role: "Web Dev Lead",
+        bio: "Developer etc.",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png",
+      },
+      {
+        name: "Aman Kumar Rai",
+        role: "Web Dev, Frontend",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-3.png",
+      },
+      {
+        name: "Debanjan Mondal",
+        role: "DevOps, Full-Stack Dev",
+        bio: "Full Stack Dev",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-10.png",
+      },
+      {
+        name: "Monami Nandy",
+        role: "Web Dev, Frontend",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-7.png",
+      },
+    ],
+  },
+  {
+    name: "Event Management Team",
+    members: [
+      {
+        name: "Annesha Das",
+        role: "",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-4.png",
+      },
+      {
+        name: "Arpita Paul",
+        role: "",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png",
+      },
+      {
+        name: "Salini Dutta",
+        role: "",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-6.png",
+      },
+      {
+        name: "Sarmistha Ghosh",
+        role: "",
+        bio: "",
+        avatar:
+          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png",
+      },
+    ],
+  },
+];
+
+export default teams;
