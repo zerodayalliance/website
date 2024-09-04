7527d1c5772c80e004990e327f1964da7a31e7ce
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-09-05 01:55:34 +0530

    fix hero, faq, about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,28 @@
 import { Text, Title, Button, Image } from "@mantine/core";
 import image from "/src/assets/aboutpic.jpeg";
 import classes from "./About.module.css";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div id="about" className={classes.wrapper}>
       <div className={classes.body}>
-        <Title className={classes.title}>About Us</Title>
-        <Text fz="md" c="dimmed">
+        <Title className={classes.title} order={1}>
+          About Us
+        </Title>
+        <Text fz="lg">
           Welcome to ZeroDay Alliance, where SNU students passionate about
           cybersecurity and ethical hacking come together to learn about the
           latest security trends and zero-day vulnerabilities, collaborate on
           projects and challenges, share resources and tools, discuss news and
           strategies, and network with future professionals in the field.
         </Text>
-
         <div className={classes.controls}>
-          <Button className={classes.control1} size="lg">
-            Join The Community
-          </Button>
+          <Link to="/teams">
+            <Button className={classes.control1} size="md">
+              Meet Our Teams
+            </Button>
+          </Link>
         </div>
       </div>
       <Image src={image} className={classes.image} />
