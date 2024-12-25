9101365bbfe96fc8bbe103932445f9aa371ce53e
monami_nandy <monaminandy16@gmail.com>
2024-12-25 21:27:54 +0530

    home page added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+
+
+export const Hero = () => {
+  return (
+    <div>
+      <h1 className="font-iceberg text-7xl -mt-20 ml-20 text-hero ">ZERODAY ALLIANCE</h1>
+      <h2 className="font-Encode_Sans text-5xl ml-20 text-onhold">Let us come together and make </h2>
+      <h2 className="font-Encode_Sans text-5xl ml-20 text-onhold">our society <span className="font-Encode_Sans text-5xl text-hero font-bold">Secure</span></h2>
+    </div>
+  );
+}
+
+export default Hero
