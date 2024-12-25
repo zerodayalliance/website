213c08145b7584f9ef8191ee53ecc169ff964623
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-12-25 18:58:57 +0530

    fix NavBar

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -21,14 +21,15 @@ export const FloatingNav = ({
   const { scrollYProgress } = useScroll();
 
   const [visible, setVisible] = useState(true);
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
     if (typeof current === "number") {
-      const positionY = scrollYProgress.get();
-      const direction = current! - positionY!;
+      let direction = current! - scrollYProgress.getPrevious()!; // eslint-disable-line
+      let positionY = scrollYProgress.get(); // eslint-disable-line
 
-      if (positionY > 0 && positionY < 0.2) {
+      if (positionY > 0 && positionY < 0.3) {
         setVisible(true);
       } else {
         if (direction < 0) {
@@ -55,8 +56,8 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          `flex items-center justify-evenly mx-auto sm:max-w-lg max-w-xs max-h-12
-          fixed top-5 px-4 py-2 inset-x-0 z-[5000] font-encode-sans
+          `flex justify-center items-center mx-auto sm:max-w-md max-w-xs max-h-12
+          fixed top-5 px-2 py-2 inset-x-0 z-[5000] font-encode-sans
           border border-transparent backdrop-blur-sm rounded-full
           dark:border-white/[0.2] dark:bg-gray-800/20 bg-white/20
           shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] `,
@@ -68,13 +69,33 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              `relative flex items-center px-3 py-1 rounded-full 
-              dark:text-neutral-50 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 
-              hover:bg-gradient-to-b from-secondary to-quaternary`
+              `relative flex justify-center items-center group px-1.5 py-1 rounded-full h-full w-full
+              dark:text-neutral-50 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500`
             )}
+            onMouseEnter={() => setHoveredIndex(idx)}
+            onMouseLeave={() => setHoveredIndex(null)}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden text-sm sm:block">{navItem.name}</span>
+            <AnimatePresence>
+              {hoveredIndex === idx && (
+                <motion.span
+                  className="absolute block inset-0 h-full w-full rounded-3xl bg-gradient-to-b from-secondary to-quaternary"
+                  layoutId="hoverBackground"
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 0.15 },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    transition: { duration: 0.15, delay: 0.2 },
+                  }}
+                />
+              )}
+            </AnimatePresence>
+            <span className="block sm:hidden z-[5100]">{navItem.icon}</span>
+            <span className="hidden text-md sm:block z-[5100]">
+              {navItem.name}
+            </span>
           </Link>
         ))}
         {/* <button className="relative font-medium text-sm px-2 py-1 border rounded-full border-neutral-200 dark:border-white/[0.2] text-black dark:text-white hover:bg-gradient-to-b from-primary to-hero">
