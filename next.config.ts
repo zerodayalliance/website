7c55da4886521c2ad09efe70c61b34e1772c2913
Raj Sekhar Paul <rajsekharpaul45@gmail.com>
2024-12-27 03:50:28 +0530

    added teams

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
+
---
+++ src/components/teams/Info.tsx
@@ -0,0 +1,17 @@
+export const Info = () => {
+  return (
+    <div className="flex justify-center mt-20 z-10">
+      <div className="text-center">
+        <h1 className="font-gidugu text-5xl text-hero">Teams</h1>
+        <h2 className="font-iceberg text-6xl mt-3 text-white">
+          Meet Our<span className="text-hero"> Core</span>!
+        </h2>
+        <h2 className="font-encodesans text-1xl mt-3 text-white ">
+        Hello everyone! We’re excited to introduce our amazing team members for the tenure 2024-2025. <br />Each of them brings unique skills and passion to the club.
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
