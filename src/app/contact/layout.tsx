9d76096d45cd712310bdb84108251f8e411c98d9
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-12-29 11:23:30 +0000

    change some structure

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact | ZeroDay Alliance SNU",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
