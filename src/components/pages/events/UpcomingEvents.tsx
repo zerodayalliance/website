33b2e70f5180d598035a25400f43d720fb67aaae
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-01-12 02:09:17 +0530

    fix light mode on events page

--- src/components/pages/events/UpcomingEvents.tsx
+++ src/components/pages/events/UpcomingEvents.tsx
@@ -7,10 +7,10 @@ export default function UpcomingEvents(props: { data: IEvent[] }) {
   return (
     <>
       <h2 className="font-iceberg text-5xl text-center mt-3">
-        <span className="text-gray-200">Upcoming</span>{" "}
+        <span className="text-gray-800 dark:text-gray-200">Upcoming</span>{" "}
         <span className="text-hero">Events</span>
       </h2>
-      <p className="font-encode-sans text-center my-4 text-gray-200">
+      <p className="font-encode-sans text-center my-4 text-gray-800 dark:text-gray-200">
         Our Upcoming Events
       </p>
       <div className="flex justify-center items-center flex-row mb-10">
