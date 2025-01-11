e04695d297cbd1d1f124c9ad884fd983d1b2ed4f
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-01-12 01:13:21 +0530

    add graphql client for events page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,19 @@ export default {
         semibold: "600",
         bold: "700",
       },
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
     },
   },
   plugins: [addVariablesForColors],
