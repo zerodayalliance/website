669441493805d8522ed52afdbae88648ce301987
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-02-16 18:44:58 +0530

    add quiz page

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,15 @@
+export default function page() {
+  return (
+    <>
+      <iframe
+        src="https://app.sli.do/event/imdThkMrKNRXCVSi3ZvEdT"
+        height="100%"
+        width="100%"
+        frameBorder="0"
+        style={{ minHeight: "100vh" }}
+        allow="clipboard-write"
+        title="Slido"
+      ></iframe>
+    </>
+  );
+}
