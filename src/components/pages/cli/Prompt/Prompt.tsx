8b4fd7eb86bac6f278f0d88e5924f5bdc4bb1f60
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-02-16 18:11:40 +0530

    fix hostName in CLI

--- src/components/pages/cli/Prompt/Prompt.tsx
+++ src/components/pages/cli/Prompt/Prompt.tsx
@@ -1,8 +1,16 @@
-import { userName, hostName, defaultDir } from "@/config/PS1";
+import { userName, defaultDir } from "@/config/PS1";
 
 import "./Prompt.modules.css";
+import { useEffect, useState } from "react";
 
 const Prompt = () => {
+  const [hostName, setHostName] = useState("");
+
+  useEffect(() => {
+    setHostName(window.location.hostname);
+    // document.title = `${userName}@${window.location.hostname}:${defaultDir}`;
+  }, []);
+
   return (
     <>
       <span className="userName">{userName}</span>
