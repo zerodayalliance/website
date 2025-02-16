8eca8e6831f3eda8cdfcce7ae95efc2876e92dfd
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-02-16 18:52:59 +0530

    add gallery & blog

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,7 @@
+export default function Gallery() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <h1 className="font-iceberg text-5xl text-quaternary">Coming Soon...</h1>
+    </div>
+  );
+}
