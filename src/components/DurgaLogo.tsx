6452048d04a29399e6f47a14f3e5a42ea3be949b
ayushghosh-123 <ghoshayush910@gmail.com>
2025-09-22 13:46:29 +0530

    now some change hero page logo

--- src/components/DurgaLogo.tsx
+++ src/components/DurgaLogo.tsx
@@ -6,14 +6,16 @@ function DurgaLogo() {
   return (
     <div>
       <Image 
-        src="/durgapujo.svg"   // path from the public folder
+        src="/durgapujo.svg"
         alt="Durga Puja Logo"
         width={800}
         height={593}
-        className="p-3 flex-grow-0 flex-shrink-0 h-38 w-38 sm:h-60 sm:w-60 md:h-60 md:w-60 "
+        className="p-3 flex-grow-0 flex-shrink-0 h-38 w-38 sm:h-60 sm:w-60 md:h-60 md:w-60 fill-current "
       />
     </div>
   )
 }
 
 export default DurgaLogo
+
+
