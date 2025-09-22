ddef9bc8163314671d14fae47760a1a9b97ec1f9
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-09-23 01:56:42 +0530

    fix some styles of teams page

--- src/components/pages/teams/Info.tsx
+++ src/components/pages/teams/Info.tsx
@@ -3,12 +3,13 @@ export const Info = () => {
     <div className="flex justify-center mt-20 z-10">
       <div className="text-center">
         <h1 className="font-gidugu text-5xl text-hero">Teams</h1>
-        <h2 className="font-iceberg text-6xl mt-3 text-white">
-          Meet Our<span className="text-hero"> Core</span>!
+        <h2 className="font-iceberg text-6xl mt-3 text-gray-800 dark:text-white">
+          Meet Our<span className="text-hero"> Core Teams</span>!
         </h2>
-        <h2 className="font-encodesans text-1xl mt-3 text-white ">
-          Hello everyone! We’re excited to introduce our amazing team members
-          for the tenure 2024-2025. <br />
+        <h2 className="font-encodesans text-xl mt-3 text-gray-800 dark:text-white ">
+          Hello everyone! We're excited to introduce our amazing Core Team
+          Members of ZeroDay Alliance.
+          <br />
           Each of them brings unique skills and passion to the club.
         </h2>
       </div>
