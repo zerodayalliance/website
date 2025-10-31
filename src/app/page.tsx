9abff24cdeb175e969fed817356633f40507ca70
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-10-31 21:22:19 +0530

    change to normal logo and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/pages/home/Hero";
 import About from "@/components/pages/home/About";
-// import Logo from "@/components/Logo";
-import FestiveLogo from "@/components/FestiveLogo";
+import Logo from "@/components/Logo";
+// import FestiveLogo from "@/components/FestiveLogo";
 import TopLines from "@/components/pages/home/TopLines";
 import BottomLines from "@/components/pages/home/BottomLines";
 import Footer from "@/components/Footer";
@@ -14,8 +14,8 @@ export default function Home() {
       <TopLines className="absolute top-0 left-0 -ml-72 -mt-5 sm:-ml-28 sm:-mt-14 md:-ml-16 md:-mt-14 opacity-60" />
       <header className="flex flex-row justify-between items-center h-lvh max-w-6xl mx-auto px-10 pt-40 pb-24 sm:pb-32 md:pb-40">
         <Hero />
-        {/* <Logo className="p-3 flex-grow-0 flex-shrink-0 h-28 w-28 sm:h-48 sm:w-48 md:h-60 md:w-60" /> */}
-        <FestiveLogo />
+        <Logo className="p-3 flex-grow-0 flex-shrink-0 h-28 w-28 sm:h-48 sm:w-48 md:h-60 md:w-60" />
+        {/* <FestiveLogo /> */}
       </header>
       <main className="max-w-6xl mx-auto px-10">
         <h1 className="font-gidugu text-6xl text-hero text-center">About Us</h1>
