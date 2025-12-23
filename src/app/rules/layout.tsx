385728f9674f3b7fc16c84d1750550a4e317ee08
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-12-24 01:01:57 +0530

    update footer

--- src/app/rules/layout.tsx
+++ src/app/rules/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Gallery | ZeroDay Alliance",
+  title: "Rules & Regulations | ZeroDay Alliance",
 };
 
 export default function RootLayout({
